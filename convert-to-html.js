e8dc73c7cd50fd4a55ef44f4a32800733f22bc23
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-02-11 15:11:34 +0100

    Add script to convert markdown to styled HTML

---
+++ convert-to-html.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const marked = require('marked');
+
+function run() {
+  if (!fs.existsSync('output')) {
+    fs.mkdirSync('output');
+  }
+
+  let template = fs.readFileSync('template.html', 'utf8');
+  let markdown = fs.readFileSync('README.md', 'utf8');
+  let html = template.replace('$$CONTENT$$', marked(markdown));
+  fs.writeFileSync('output/rules.html', html, 'utf8');
+
+  let styles = fs.readFileSync('styles.css', 'utf8');
+  let normalize = fs.readFileSync(require.resolve('normalize.css'), 'utf8');
+  fs.writeFileSync('output/styles.css', normalize + styles, 'utf8');
+
+  fs.copyFileSync('logo.jpg', 'output/logo.jpg');
+}
+
+run();
